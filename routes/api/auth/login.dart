994198cf1abe62ff96a32b32d4b0fbf8039de2d5
ZianF <zianfahrudy@gmail.com>
2025-12-17 18:33:29 +0800

    feat(auth): update

--- routes/api/auth/login.dart
+++ routes/api/auth/login.dart
@@ -7,49 +7,62 @@ import 'package:auth_feature/services/auth_service.dart';
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  final authService = context.read<AuthService>();
-
-  final body = await context.request.json() as Map<String, dynamic>;
-  final username = body['username'] as String;
-  final passwordInput = body['password'] as String; // Password ketikan user
-
-  final repo = context.read<AuthRepository>();
-
-  try {
-    // 1. Cari user berdasarkan username
-    final user = await repo.findByUsername(username);
-
-    final isPasswordCorrect =
-        authService.checkPassword(passwordInput, user.password);
-    if (!isPasswordCorrect) {
-      return Response.json(
-        statusCode: HttpStatus.badRequest,
-        body: {
-          'message': 'Invalid credentials',
-        },
-      );
-    }
-
-    final token = authService.generateToken(user.id.toString(), user.username);
-    final refreshToken =
-        authService.generateRefreshToken(user.id.toString(), user.username);
-
-    return Response.json(
-      body: {
-        'message': 'Login Successfully',
-        'data': {
-          'token': token,
-          'refresh_token': refreshToken,
-          'user': {
-            'id': user.id,
-            'username': user.username,
-          },
+  return Response.json(
+    body: {
+      'message': 'Login Successfully',
+      'data': {
+        'token': 'token',
+        'refresh_token': 'refreshToken',
+        'user': {
+          'id': 'user.id',
+          'username': 'user.username',
         },
       },
-    );
-  } catch (e) {
-    return Response.json(statusCode: 400, body: {'error': e.toString()});
-  }
+    },
+  );
+  // final authService = context.read<AuthService>();
+
+  // final body = await context.request.json() as Map<String, dynamic>;
+  // final username = body['username'] as String;
+  // final passwordInput = body['password'] as String; // Password ketikan user
+
+  // final repo = context.read<AuthRepository>();
+
+  // try {
+  //   // 1. Cari user berdasarkan username
+  //   final user = await repo.findByUsername(username);
+
+  //   final isPasswordCorrect =
+  //       authService.checkPassword(passwordInput, user.password);
+  //   if (!isPasswordCorrect) {
+  //     return Response.json(
+  //       statusCode: HttpStatus.badRequest,
+  //       body: {
+  //         'message': 'Invalid credentials',
+  //       },
+  //     );
+  //   }
+
+  //   final token = authService.generateToken(user.id.toString(), user.username);
+  //   final refreshToken =
+  //       authService.generateRefreshToken(user.id.toString(), user.username);
+
+  //   return Response.json(
+  //     body: {
+  //       'message': 'Login Successfully',
+  //       'data': {
+  //         'token': token,
+  //         'refresh_token': refreshToken,
+  //         'user': {
+  //           'id': user.id,
+  //           'username': user.username,
+  //         },
+  //       },
+  //     },
+  //   );
+  // } catch (e) {
+  //   return Response.json(statusCode: 400, body: {'error': e.toString()});
+  // }
 
   // 2. Cek User & Password
 
